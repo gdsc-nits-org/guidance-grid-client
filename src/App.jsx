625577f5d627e4c2d49f6c1ca0a5c789a59f82ea
Pratik Majumdar <info.pratikmajumdar@gmail.com>
2022-11-19 01:07:27 +0530

    SETUP: React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { Home } from "./Pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Navbar } from "./Components";
+
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,3 +1,4 @@
 import Button from "./Button/Button";
+import Navbar from "./Navbar/Navbar";
 
-export { Button };
+export { Button, Navbar };
