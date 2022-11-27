63d88c0d7c08aacdcf2761530d9f7c562eb2976e
Jaydeep Das <jaydeepjd.1125@gmail.com>
2022-11-28 02:43:54 +0530

    Article Card (#6)
    * ADD: Root Colors
    
    * ADD: article card component
    
    This commit adds a minimal ArticleCard component along with TagItem Component
    which can be used to simply create tags for articles.
    
    * ADD: bookmark icon
    
    * FIX: Resolved Reviews and added basic loading screen
    
    Co-authored-by: Saurav Pal <resyfer.dev@gmail.com>

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,4 +1,7 @@
 import Button from "./Button/Button";
 import Navbar from "./Navbar/Navbar";
+import ArticleCard from "./ArticleCard/ArticleCard";
+import TagItem from "./TagItem/TagItem";
+import Loading from "./Loading/Loading";
 
-export { Button, Navbar };
+export { Button, Navbar, ArticleCard, TagItem, Loading };
