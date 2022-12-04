7192c8aef7f5a391efd9c6451a7d495c2b8a1688
Kherkatary <kherkatarysan456@gmail.com>
2022-12-04 19:34:25 +0530

    sidebarData json added, map key added

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,55 +1,34 @@
+import { useState } from "react";
 import styles from "./SideBar.module.scss";
-
+import sidebarData from "./sidebarData.json";
 const SideBar = () => {
-  const tags = [
-    "node",
-    "Vue Js",
-    "react Js",
-    "placements",
-    "django",
-    "Java",
-    "embedded systems",
-    "kotlin",
-    "express",
-    "SQL",
-    "Systenm architect",
-    "Data Structures",
-    "Ruby",
-    "pycharm",
-    "typescript",
-  ];
-
-  const bookmarkArray = [
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, expedita!",
-    "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-    "Lorem ipsum dolor sit amet consectetur.",
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    "Lorem ipsum dolor sit amet.",
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    "orem ipsum dolor sit amet consectetur.",
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing.",
-  ];
+  const [tagArray] = useState(sidebarData.tag);
+  const [bookmarkArray] = useState(sidebarData.bookmark);
 
   return (
     <div className={styles.main}>
       <div className={styles.searchTags}>
         <h4>Search by tags</h4>
-        {tags.slice(0, 15).map((tag) => (
-          <span>
-            <a href="/">{tag}</a>
-          </span>
-        ))}
+        {tagArray.slice(0, 15).map((tag) => {
+          return (
+            <span>
+              <a href="/" key={tag.id}>
+                {tag.tag}
+              </a>
+            </span>
+          );
+        })}
       </div>
 
       <div className={styles.bookmarks}>
-        <h4>Bookmarks</h4>
-        {bookmarkArray.slice(0, 5).map((mark) => (
-          <a href="/">{mark}</a>
-        ))}
+        <h4>Search by tags</h4>
+        {bookmarkArray.slice(0, 5).map((mark) => {
+          return (
+            <a href="/" key={mark.markedContent}>
+              {mark.markedContent}
+            </a>
+          );
+        })}
 
         <a href="/">Read more</a>
       </div>
