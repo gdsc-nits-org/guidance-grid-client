ff1d514e87576bf79a04df716e84769d78609745
Aditya <kumar.31aditya@gmail.com>
2022-12-07 03:12:21 +0530

    improved trend cards

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -4,13 +4,18 @@ import styles from "./Trending.module.scss";
 const Trending = ({ data = {} }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.metadata}>
-        <div className={styles.rank}>0{data?.rank}</div>
+      <div className={styles.rank}>0{data?.rank}</div>
+      <div className={styles.profile}>
         <Link className={styles.author} to={data?.author?.profile}>
           <img height="20" src={data?.author?.image} alt="Profile of author" />
+        </Link>
+      </div>
+
+      <div className={styles.metadata}>
+        <Link className={styles.author} to={data?.author?.profile}>
           <span className={styles.bold}>{data?.author?.name}</span>
         </Link>
-        in
+        {"  in "}
         <Link className={styles.author} to={data?.domain?.url}>
           <span className={styles.bold}>{data?.domain?.name}</span>
         </Link>
@@ -18,10 +23,10 @@ const Trending = ({ data = {} }) => {
       <div className={styles.heading}>
         <Link to={data?.url}>{data?.title}</Link>
       </div>
-      <div className={styles.metadata}>
+      <div className={styles.date}>
         <div>{data?.date} •</div>
-        <div>10 mins read</div>
       </div>
+      <div className={styles.time}>10 mins read</div>
     </div>
   );
 };
