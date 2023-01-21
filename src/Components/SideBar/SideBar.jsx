be2d49e6ccd05081865ea3b9fd5f375c66ed5baf
Kherkatary <kherkatarysan456@gmail.com>
2023-01-21 23:21:10 +0530

    FIX: use useFetchData hook in SideBar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useFetchData } from "../../Hooks";
 import styles from "./SideBar.module.scss";
-import sidebarData from "./sidebarData.json";
+// mport sidebarData from "./sidebarData.json";
 
 const SideBar = () => {
-  const [tagArray] = useState(sidebarData.tag);
-  const [bookmarkArray] = useState(sidebarData.bookmark);
+  const [tagsRespState] = useFetchData("/articles/sidebarData.json");
 
   return (
     <div className={styles.main}>
       <div className={styles.searchTags}>
         <h4>Search by tags</h4>
-        {tagArray.slice(0, 15).map((tag, idx) => {
-          return (
-            <span key={idx}>
-              <Link to="/">{tag.tag}</Link>
-            </span>
-          );
-        })}
+        {tagsRespState.loading ||
+          tagsRespState.response.tags.slice(0, 15).map((tag, idx) => {
+            return (
+              <span key={idx}>
+                <Link to="/">{tag.tag}</Link>
+              </span>
+            );
+          })}
       </div>
 
       <div className={styles.bookmarks}>
         <h4>Search by tags</h4>
-        {bookmarkArray.slice(0, 5).map((mark) => {
-          return (
-            <Link to="/" key={mark.id}>
-              {mark.markedContent}
-            </Link>
-          );
-        })}
+        {tagsRespState.loading ||
+          tagsRespState.response.bookmark.slice(0, 5).map((mark) => {
+            return (
+              <Link to="/" key={mark.id}>
+                {mark.markedContent}
+              </Link>
+            );
+          })}
 
         <Link href="/">Read more</Link>
       </div>
