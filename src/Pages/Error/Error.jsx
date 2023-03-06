09498924fdcc1e2021229e402e6936b6df9b249d
SHIBAM <shibamdebnath07@gmail.com>
2023-03-07 01:16:44 +0530

    ADD: error 404 page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,21 @@
+import React from "react";
+import Lottie from "react-lottie";
 import style from "./Error.module.scss";
+import error404 from "../../../public/error/error404.json";
 
 const Error = ({ error }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: error404,
+  };
+
   return (
     <main className={style.error}>
-      <h1>{error}</h1>
+      <div className={style.lottie}>
+        <Lottie options={defaultOptions} />
+      </div>
+      <h1 className={style.errorMessage}>{error}</h1>
     </main>
   );
 };
